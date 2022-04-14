ed1179712e2b58c9e509925e693318ccc6c12cb0
vinapro <vinapro91@gmail.com>
2022-04-14 18:23:12 -0300

    fix: add typing in error messager

--- src/controllers/user/userController.ts
+++ src/controllers/user/userController.ts
@@ -2,15 +2,21 @@ import { Request, Response } from "express";
 
 import { CreateUserService } from "../../services";
 
-const CrateUserController = async (request: Request, response: Response) => {
+const CrateUserController = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
   try {
     const userData = request.body;
     const user = await CreateUserService(userData);
     return response.json(user);
-  } catch (error) {
-    return response.status(400).json({
-      message: error.message || "Unexpected error.",
-    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return response.status(400).json({
+        message: error.message || "Unexpected error.",
+      });
+    }
+    return response.status(500).json({ Error: "Unexpected error." });
   }
 };
 
