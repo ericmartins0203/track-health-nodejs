ed3918c3f3c31fa806e3e8631bcb9e31d202d64f
Matheus <matheus.g.almeida21@gmail.com>
2022-04-15 18:16:19 -0300

    fix: remove console logs

--- src/middlewares/validate/validateShape.ts
+++ src/middlewares/validate/validateShape.ts
@@ -5,10 +5,7 @@ const validateShape =
   (shape: AnySchema) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log(req.body);
-
       req.validate = await shape.validate(req.body);
-      console.log(req.validate);
       return next();
     } catch (error: any) {
       return res.status(400).json({ error: error.errors });
