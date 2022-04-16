0ad3a8e336780417205306ed8fa90c336702a5db
vinapro <vinapro91@gmail.com>
2022-04-16 14:29:37 -0300

    feat: create vaccine Entity

---
+++ src/entities/index.ts
@@ -0,0 +1,6 @@
+import { ProfileImage } from "./user/ProfileImage";
+import { User } from "./user/User";
+import { UserVaccine } from "./vaccine/userVaccine";
+import { Vaccine } from "./vaccine/vaccine";
+
+export { Vaccine, UserVaccine, ProfileImage, User };
