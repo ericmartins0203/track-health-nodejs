37d82412b15ff3e2bff1fe0eb6a2e7edce5511f4
Eric Esteves Martins <estevesmartinseric@gmail.com>
2022-04-18 11:30:02 -0300

    fix: problem in route delete disease when passing wrong id

--- src/controllers/userDisease/deleteUserDiseaseController.ts
+++ src/controllers/userDisease/deleteUserDiseaseController.ts
@@ -3,15 +3,24 @@ import { Request, Response } from "express";
 import { deleteUserDiseaseService } from "../../services";
 
 const deleteUserDiseaseController = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { userId } = req.decoded;
+  try {
+    const { id } = req.params;
+    const { userId } = req.decoded;
 
-  const userDisease = await deleteUserDiseaseService(id, userId);
-  const disease = userDisease.filter((userDisease) => userDisease.id === id);
+    const userDisease = await deleteUserDiseaseService(id, userId);
+    const disease = userDisease.filter((userDisease) => userDisease.id === id);
 
-  res.status(200).json({
-    message: `${disease[0].disease.name} deleted`,
-  });
+    return res.status(200).json({
+      message: `${disease[0].disease.name} deleted`,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+    return res.status(500).json({ Error: "Unexpected error." });
+  }
 };
 
 export default deleteUserDiseaseController;
--- src/services/user_disease/deleteUserDiseaseService.ts
+++ src/services/user_disease/deleteUserDiseaseService.ts
@@ -1,19 +1,13 @@
 import { UserDiseaseRepository } from "../../repositories";
 
 const deleteUserDiseaseService = async (id: string, userId: string) => {
-  try {
-    const userDisease = await new UserDiseaseRepository().getUserDisease(
-      userId
-    );
-    if (!userDisease.filter((disease) => disease.id === id)) {
-      throw new Error("UserDisease not found");
-    }
+  const userDisease = await new UserDiseaseRepository().getUserDisease(userId);
 
-    await new UserDiseaseRepository().deleteUserDisease(id);
-    return userDisease;
-  } catch (err) {
-    console.log(err);
+  if (!userDisease.find((disease) => disease.id === id)) {
     throw new Error("UserDisease not found");
   }
+
+  await new UserDiseaseRepository().deleteUserDisease(id);
+  return userDisease;
 };
 export default deleteUserDiseaseService;
